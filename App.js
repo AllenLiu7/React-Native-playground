915bcef76c03f6cce68cf6fdf67b8f5d7d033e75
Allen Liu <allen6qi@gmail.com>
2020-08-11 22:17:18 +0800

    added list style and applied scrollview to list

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+} from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
@@ -24,11 +31,13 @@ export default function App() {
         />
         <Button title='ADD' onPress={addGoalHandler} />
       </View>
-      <View>
+      <ScrollView>
         {courseGoals.map((goal) => (
-          <Text key={goal}>{goal}</Text>
+          <View key={goal} style={styles.listItem}>
+            <Text>{goal}</Text>
+          </View>
         ))}
-      </View>
+      </ScrollView>
     </View>
   );
 }
@@ -47,5 +56,9 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     padding: 10,
+  },
+  listItem: {
+    padding: 10,
+    backgroundColor: '#ccc',
   },
 });
